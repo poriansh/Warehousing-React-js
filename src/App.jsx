aa0b49a9d0d1f69cd771f7d486ce745da091088c
poria neyshaboriyan <poria.nsh@gmail.com>
2025-02-12 13:34:54 +0330

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import "./App.css";
+import Header from "./ui/Header";
 
 function App() {
-  return <div className="flex">ds</div>;
+  return (
+    <div className="bg-slate-800 h-screen">
+      <Header/>
+      <div className="container">
+
+      </div>
+    </div>
+  );
 }
 
 export default App;
