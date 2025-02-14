bfddf3d53027b625e60c53b57370f2ed1d504792
poria neyshaboriyan <poria.nsh@gmail.com>
2025-02-14 21:14:51 +0330

    test

--- src/ui/Selector.jsx
+++ src/ui/Selector.jsx
@@ -3,10 +3,10 @@ function Selector({value, onChange, option}) {
     <select
       value={value}
       onChange={onChange}
-      className="bg-transparent cursor-pointer p-3  focus:outline-none rounded-xl border-2 border-slate-500 text-slate-400 w-50 "
+      className="bg-transparent cursor-pointer focus:ring-0 focus:border-blue-800   focus:outline-none rounded-xl border-2 border-slate-500 text-slate-400 w-50 "
     >
       {option.map((item) => (
-        <option className="bg-slate-500 text-slate-300" key={item?.id || item?.value} value={item.title}>
+        <option className="bg-slate-500  text-slate-300" key={item?.id || item?.value} value={item.title}>
           {item.title}
         </option>
       ))}
